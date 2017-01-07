e3d9ced2206a54ef88dfd3bec1da8b94287c56f6
Juokjulo <elisabeth.mansel@web.de>
2017-01-07 18:45:26 +0100

    newsletter field in user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :password_confirmation, :newsletter_abo])
   end
 
 end
