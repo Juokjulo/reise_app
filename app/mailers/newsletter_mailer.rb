9a290e0d444e60858be744de260b42c881536286
Juokjulo <elisabeth.mansel@web.de>
2018-01-13 15:55:51 +0100

    newsletter test

--- app/mailers/newsletter_mailer.rb
+++ app/mailers/newsletter_mailer.rb
@@ -2,9 +2,10 @@ class NewsletterMailer < ActionMailer::Base
   default from: 'from@example.com'
  
   
-    def welcome_email(user)
+    def welcome_email(user, newsletter)
       @user = user
+      @newsletter = newsletter
       @url  = 'localhost:3000' 
-      mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+      mail(to: @user.email, subject: @newsletter.title)
    end
 end
